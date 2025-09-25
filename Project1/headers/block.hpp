e81859d1be85ea10e20f9a86d5e91088522f26b8
Goh Jun Keat <junkeatgoh@gmail.com>
2025-09-25 22:50:08 +0800

    add readRecord() and other functions; edit functions

--- Project1/headers/block.hpp
+++ Project1/headers/block.hpp
@@ -8,10 +8,11 @@ const size_t BLOCK_SIZE = 4096; // Size of each block in bytes
 const size_t MAX_RECORDS_PER_BLOCK = BLOCK_SIZE / sizeof(Record); // Maximum records per block
 
 struct Block {
-    Record records[MAX_RECORDS_PER_BLOCK];    // 4096 / 24 = 170 records per block
     int num_records = 0;    // Number of records currently in the block
+    Record records[MAX_RECORDS_PER_BLOCK];    // 4096 / 24 = 170 records per block
 };
 
-bool isBlockFull(const Block& block, int max_records);
+bool isBlockFull(const Block& block);
 void resetBlockNumRecords(Block& block);
-
+Record readRecord(const Block& block, size_t index);
+void printRecord(const Record& rec);
